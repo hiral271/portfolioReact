89841784b9d3eb13ce6a883bd50f07487d1346f3
Hiral m patel <hiralm227@gmail.com>
2021-04-10 22:37:57 -0400

    project page

--- portfolio/src/pages/About/About.js
+++ portfolio/src/pages/About/About.js
@@ -10,15 +10,17 @@ import "./about.css"
 // }
 
 const About = () => (
-  <div id="about">
-    <h1>About Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+  <div className="jumbotron">
+<h1 className="display-4">Hiral Mohan Patel</h1>
+<h2>Codes i am similiar with</h2>
+<ul>
+  <li>
+  <img src="" alt="Bootstrap" width="32" height="32"></img>
+  </li>
+</ul>
+<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+ 
+   
   </div>
 );
 
